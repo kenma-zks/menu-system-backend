c6105dee3044844fd75a3bd236553232ed49f9ea
kenma-zks <pallawmgr20@gmail.com>
2023-03-18 19:12:15 +0545

    add views for past booking list and details

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,14 @@
 from .serializers import UserSerializer
 from .models import Customer
 from rest_framework import generics
+from django.utils import timezone
 
 class ListRegisterView(generics.ListAPIView):
-  queryset = Customer.objects.all()
+  queryset = Customer.objects.filter(booking_date__gte=timezone.now().date())
+  serializer_class = UserSerializer
+
+class ListPastRegisterView(generics.ListAPIView):
+  queryset = Customer.objects.filter(booking_date__lt=timezone.now().date())
   serializer_class = UserSerializer
 
 #Class based view to register user
@@ -12,5 +17,9 @@ class RegisterView(generics.CreateAPIView):
   serializer_class = UserSerializer
 
 class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Customer.objects.all()
+  serializer_class = UserSerializer
+
+class PastBookingDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Customer.objects.all()
   serializer_class = UserSerializer
