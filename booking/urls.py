8ec6020b1460bfa46ee240d783e99c704e311cf6
kenma-zks <pallawmgr20@gmail.com>
2023-03-18 19:11:20 +0545

    add paths for past booking list and details

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView, ListRegisterView, BookingDetail
+from .views import RegisterView, ListRegisterView, BookingDetail, ListPastRegisterView, PastBookingDetail
 
 urlpatterns = [
   path('register/',RegisterView.as_view()),
   path('list/',ListRegisterView.as_view()),
   path('list/<int:pk>/',BookingDetail.as_view()),
+  path('list/past/',ListPastRegisterView.as_view()),
+  path('list/past/<int:pk>/',PastBookingDetail.as_view()),
 ]
